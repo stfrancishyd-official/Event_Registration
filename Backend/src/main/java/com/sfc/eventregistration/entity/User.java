1e2ccd1200df7e117f4e269c5a299067758ffed2
gladythomas <gladychaprath1999@gmail.com>
2026-02-10 23:31:31 +0530

    added bcrypt in config for hashing

--- Backend/src/main/java/com/sfc/eventregistration/entity/User.java
+++ Backend/src/main/java/com/sfc/eventregistration/entity/User.java
@@ -8,9 +8,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name="users")
+@Getter
+@Setter
 public class User {
     
     @Id
