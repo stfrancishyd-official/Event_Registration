7996dfcbaec2f7a6f044c72f43360b8967888554
gladythomas <gladychaprath1999@gmail.com>
2026-02-11 20:58:10 +0530

    Add controller Login/register

---
+++ Backend/src/main/java/com/sfc/eventregistration/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.sfc.eventregistration.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sfc.eventregistration.dto.RegisterRequestDto;
+import com.sfc.eventregistration.service.AuthService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("api/v1/auth")
+@CrossOrigin
+public class AuthController {
+    
+    private final AuthService authService;
+
+    public AuthController(AuthService authService){
+        this.authService=authService;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody  RegisterRequestDto request) {
+        
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequestDto request) {
+        return authService.login(request);
+        
+    }
+    
+    
+
+}
