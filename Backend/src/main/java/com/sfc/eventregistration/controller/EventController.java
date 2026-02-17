f8ff09f451063b710113081ffaca5c62f142d718
gladythomas <gladychaprath1999@gmail.com>
2026-02-17 20:39:43 +0530

    feat: setup Event module with proper constructor injection

--- Backend/src/main/java/com/sfc/eventregistration/controller/EventController.java
+++ Backend/src/main/java/com/sfc/eventregistration/controller/EventController.java
@@ -1,5 +1,33 @@
 package com.sfc.eventregistration.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sfc.eventregistration.entity.Event;
+import com.sfc.eventregistration.service.EventService;
+
+@RestController
+@RequestMapping("/api/v1/events")
+
 public class EventController {
-    
+            
+    private final EventService eventService = null;
+
+    // faculty creates events
+    @PostMapping
+      public Event createEvent(@RequestBody Event event){
+                return eventService.createdEvent(event);
+      }
+
+      // Student View events
+      @GetMapping
+      public List<Event> getAllEvents(){
+        return eventService.getAllEvents();
+      }
+
 }
